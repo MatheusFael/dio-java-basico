5cb487157e042116fe12b28be2a5a88a48a57646
MatheusFael <matheusfaelsonvalerio@gmail.com>
2025-04-03 23:21:48 -0300

    new project conta bancario added

--- conta-bancario/src/ContaBanco.java
+++ conta-bancario/src/ContaBanco.java
@@ -1,8 +1,21 @@
-
-
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaBanco {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.print("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); 
+        System.out.print("Digite o numero da agencia ");
+        String numeroAgencia = scanner.nextLine();
+        System.out.print("Digite o nome do cliente: "); 
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Digite o saldo inicial: ");
+        double saldo = scanner.nextDouble();
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " 
+            + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + String.format("%.2f", saldo) 
+            + " já está disponível para saque.");
+
     }
 }
